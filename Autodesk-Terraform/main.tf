9b0a93414e475da2bb092d2cece075aae25d7a66
skoduri <106002018+sreedharkoduri@users.noreply.github.com>
2024-08-02 15:49:53 +0530

    Update main.tf

---
+++ Autodesk-Terraform/main.tf
@@ -0,0 +1,10 @@
+resource "aws.instance" "Autodesk-Instance" {
+    ami = "ami-0f58b397bc5c1f2e8"
+    instance_type = "t2.micro"
+    key_name = "sreetest.pem"
+    tags 
+     {
+        name = "Autodesk-Instance"
+     }
+
+}
